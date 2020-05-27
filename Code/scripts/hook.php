247cb2aaccc5ecbdd0effccdeb9f8726ed5f033f
gody243 <40236375+gody243@users.noreply.github.com>
2020-05-27 13:28:38 +0100

    Create hook.php

---
+++ Code/scripts/hook.php
@@ -0,0 +1,28 @@
+<?php
+$directory='../assets/img';
+
+$allowed_extensions=array('png');
+
+$file_parts=array();
+
+$response="";
+
+$dir_handle=opendir($directory);
+
+while($file=readdir($dir_handle)){
+	if(substr($file,0,1)!='.'){
+		
+		$file_components=explode('.',$file);
+		
+		$extension=strtolower(array_pop($file_components));
+		
+		if(in_array($extension,$allowed_extensions))
+		{
+			$response.=$directory.'/'.$file.'~';
+		}
+	}
+}
+closedir($dir_handle);
+
+echo substr_replace($response,"",-1);
+?>
